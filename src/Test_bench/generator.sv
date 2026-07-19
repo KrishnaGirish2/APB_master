d01a3bbd109bbaebf24b98e6007d80f4091378de
KrishnaGirish2 <krishnagirish1458@gmail.com>
2026-07-19 13:28:48 +0530

    Update generator.sv

--- src/Test_bench/generator.sv
+++ src/Test_bench/generator.sv
@@ -1,81 +1,97 @@
 `include "defines.sv"
 class apb_generator;
-apb_transaction blueprint;
-mailbox #(apb_transaction)mbx_gd;
-function new(mailbox #(apb_transaction)mbx_gd);
-this.mbx_gd=mbx_gd;
-blueprint=new();
-endfunction
+  apb_transaction blueprint;
+  mailbox #(apb_transaction)mbx_gd;
+  function new(mailbox #(apb_transaction)mbx_gd);
+    this.mbx_gd=mbx_gd;
+    blueprint=new();
+  endfunction
+  task start();
+    blueprint.transfer=0;
+    blueprint.write_read=0;
+    blueprint.wdata_in=8'd0;
+    blueprint.strb_in=4'b0000;
+    blueprint.PSLVERR=0;
+    blueprint.PREADY=0;
+    mbx_gd.put(blueprint.copy());
 
-task start();
+    blueprint.transfer=1;
+    blueprint.write_read=0;
+    blueprint.PRDATA=32'h0;
+    blueprint.strb_in=0;
+    blueprint.PSLVERR=0;
+    blueprint.PREADY=1;
+    mbx_gd.put(blueprint.copy());
+    
+    blueprint.transfer=1;
+    blueprint.write_read=0;
+    blueprint.PRDATA=32'hFFFFFFFF;
+    blueprint.strb_in=0;
+    blueprint.PSLVERR=0;
+    blueprint.PREADY=1;
+    mbx_gd.put(blueprint.copy());
+    
+    blueprint.transfer=1;
+    blueprint.write_read=1;
+    blueprint.PRDATA=32'd20;
+    blueprint.strb_in=0;
+    blueprint.PSLVERR=0;
+    blueprint.PREADY=1;
+    mbx_gd.put(blueprint.copy());
+    
+    blueprint.transfer=1;
+    blueprint.write_read=0;
+    blueprint.addr_in=8'd20;
+    blueprint.PRDATA=32'd20;
+    blueprint.strb_in=0;
+    blueprint.PSLVERR=0;
+    blueprint.PREADY=1;
+    mbx_gd.put(blueprint.copy());
+    
+    blueprint.transfer=1;
+    blueprint.write_read=0;
+    blueprint.PRDATA=32'hFFFFFF55;
+    blueprint.strb_in=0;
+    blueprint.PSLVERR=0;
+    blueprint.PREADY=1;
+    mbx_gd.put(blueprint.copy());
+    
+    blueprint.transfer=1;
+    blueprint.write_read=0;
+    blueprint.PRDATA=32'h1234ABAA;
+    blueprint.strb_in=0;
+    blueprint.PSLVERR=0;
+    blueprint.PREADY=1;
+    mbx_gd.put(blueprint.copy());
 
+    blueprint.transfer=1;
+    blueprint.write_read=1;
+    blueprint.wdata_in=8'd98;
+    blueprint.strb_in=4'b1111;
+    blueprint.PSLVERR=1;
+    blueprint.PREADY=1;
+    mbx_gd.put(blueprint.copy());
 
-blueprint.transfer=0;
-blueprint.write_read=1;
-blueprint.wdata_in=8'd98;
-blueprint.strb_in=4'b1000;
-blueprint.PSLVERR=1;
-blueprint.PREADY=1;
-mbx_gd.put(blueprint.copy());
+    blueprint.transfer=1;
+    blueprint.write_read=1;
+    blueprint.wdata_in=8'd98;
+    blueprint.strb_in=4'b1000;
+    blueprint.PSLVERR=1;
+    blueprint.PREADY=0;
+    mbx_gd.put(blueprint.copy());
 
-blueprint.transfer=1;
-blueprint.write_read=0;
-blueprint.rdata_out=32'h0;
-blueprint.strb_in=0;
-blueprint.PSLVERR=0;
-blueprint.PREADY=1;
-mbx_gd.put(blueprint.copy());
-blueprint.transfer=1;
-blueprint.write_read=0;
-blueprint.rdata_out=32'hFFFFFFFF;
-blueprint.strb_in=0;
-blueprint.PSLVERR=0;
-blueprint.PREADY=1;
-mbx_gd.put(blueprint.copy());
-blueprint.transfer=1;
-blueprint.write_read=0;
-blueprint.rdata_out=32'hFFFFFF55;
-blueprint.strb_in=0;
-blueprint.PSLVERR=0;
-blueprint.PREADY=0;
-mbx_gd.put(blueprint.copy());
-blueprint.transfer=1;
-blueprint.write_read=0;
-blueprint.rdata_out=32'h1234ABAA;
-blueprint.strb_in=0;
-blueprint.PSLVERR=0;
-blueprint.PREADY=1;
-mbx_gd.put(blueprint.copy());
+    blueprint.transfer=1;
+    blueprint.write_read=1;
+    blueprint.wdata_in=8'd88;
+    blueprint.PSLVERR=0;
+    blueprint.PREADY=1;
+    mbx_gd.put(blueprint.copy());
 
-blueprint.transfer=0;
-blueprint.write_read=1;
-blueprint.wdata_in=8'd98;
-blueprint.strb_in=4'b1111;
-blueprint.PSLVERR=1;
-blueprint.PREADY=1;
-mbx_gd.put(blueprint.copy());
-
-blueprint.transfer=1;
-blueprint.write_read=1;
-blueprint.wdata_in=8'd98;
-blueprint.strb_in=0;
-blueprint.PSLVERR=1;
-blueprint.PREADY=0;
-mbx_gd.put(blueprint.copy());
-
-blueprint.transfer=1;
-blueprint.write_read=1;
-blueprint.wdata_in=8'd88;
-blueprint.PSLVERR=0;
-blueprint.PREADY=1;
-mbx_gd.put(blueprint.copy());
-
-for(int i=0;i<`no_of_trans;i++) begin
-blueprint.randomize();
-mbx_gd.put(blueprint.copy());
-$display("Generator Randomized transaction %0d",i+1);
-$display("transfer=%d write_read=%d addr_in=%d wdata_in=%d strb_in=%d time=%0d",blueprint.transfer, blueprint.write_read,blueprint.addr_in, blueprint.wdata_in, blueprint.strb_in, $time);
-end
-endtask
+    for(int i=0;i<`no_of_trans;i++) begin
+      blueprint.randomize();
+      mbx_gd.put(blueprint.copy());
+      $display("Generator Randomized transaction %0d",i+1);
+      $display("transfer=%d write_read=%d addr_in=%d wdata_in=%d strb_in=%d time=%0d",blueprint.transfer, blueprint.write_read,blueprint.addr_in, blueprint.wdata_in, blueprint.strb_in, $time);
+    end
+  endtask
 endclass
-
