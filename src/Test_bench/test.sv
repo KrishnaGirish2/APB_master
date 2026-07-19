951047d901d21630bcb4d6dde5af4b201e0fb7de
KrishnaGirish2 <krishnagirish1458@gmail.com>
2026-07-19 13:41:47 +0530

    Update test.sv

--- src/Test_bench/test.sv
+++ src/Test_bench/test.sv
@@ -1,16 +1,14 @@
 class apb_test;
-virtual apb_if drv_vif;
-virtual apb_if mon_vif;
-virtual apb_if ref_vif;
-apb_environment env;
-function new(virtual apb_if drv_vif, virtual apb_if mon_vif, virtual apb_if ref_vif);
-this.drv_vif=drv_vif;
-this.mon_vif=mon_vif;
-this.ref_vif=ref_vif;
-endfunction
-task run();
-env=new(drv_vif,mon_vif,ref_vif);
-env.build();
-env.start();
-endtask
+  virtual apb_if drv_vif;
+  virtual apb_if mon_vif;
+  apb_environment env;
+  function new(virtual apb_if drv_vif, virtual apb_if mon_vif);
+    this.drv_vif=drv_vif;
+    this.mon_vif=mon_vif;
+  endfunction
+  task run();
+    env=new(drv_vif,mon_vif);
+    env.build();
+    env.start();
+  endtask
 endclass
